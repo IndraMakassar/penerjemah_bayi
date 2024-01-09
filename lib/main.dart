02e3f8d712fddb654aefd08b9060c2bc3bd56ad1
hendra0123 <nerstuff24@gmail.com>
2024-01-09 15:55:01 +0800

    tesss

--- lib/main.dart
+++ lib/main.dart
@@ -5,65 +5,71 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('List Artikel'),
+        ),
+        body: ArticleList(),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
+class ArticleList extends StatelessWidget {
+  final List<Article> articles = [
+    Article('Judul Artikel 1', 'Deskripsi artikel 1', 'gambar1.jpg'),
+    Article('Judul Artikel 2', 'Deskripsi artikel 2', 'gambar2.jpg'),
+    Article('Judul Artikel 3', 'Deskripsi artikel 3', 'gambar3.jpg'),
+    // Tambahkan artikel sesuai kebutuhan
+  ];
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+    return ListView.builder(
+      itemCount: articles.length,
+      itemBuilder: (context, index) {
+        return ArticleCard(articles[index]);
+      },
+    );
+  }
+}
+
+class ArticleCard extends StatelessWidget {
+  final Article article;
+
+  const ArticleCard(this.article, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      margin: const EdgeInsets.all(8.0),
+      child: Column(
+        children: [
+          Image.asset(
+            article.imageUrl,
+            fit: BoxFit.cover,
+            width: double.infinity,
+            height: 150.0,
+          ),
+          ListTile(
+            title: Text(article.title),
+            subtitle: Text(article.description),
+          ),
+        ],
       ),
     );
   }
+}
+
+class Article {
+  final String title;
+  final String description;
+  final String imageUrl;
+
+  Article(this.title, this.description, this.imageUrl);
 }
